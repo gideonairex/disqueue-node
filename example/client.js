fb0c7abc897d39a8de1c82ad1bb2fec7b11f7bee
Gideon Rosales <gideonrosales@gmail.com>
2015-07-20 17:09:18 +0800

    feature: client server disqueue

---
+++ example/client.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Disqueue = require( '../lib' );
+var colors   = require( 'colors' );
+
+new Disqueue()
+	.then( function( disqueue ) {
+
+		process.stdin.on( 'data', function () {
+
+			for ( var i = 0 ; i< 10; i++ ) {
+
+				disqueue.request( 'v1.users.get', 'HELLO ' + i, 0 )
+					.then( function ( user ) {
+
+						return user;
+
+					} )
+					.then( function ( user ) {
+
+						return disqueue.request( 'v1.users.save', user, 0 );
+
+					} )
+					.then( function( result ) {
+						console.log( colors.green( '----------SUCCESS-----------' ) );
+						console.log( result );
+						console.log( colors.green( '----------SUCCESS-----------' ) );
+					} )
+					.catch( function ( error ) {
+
+						console.log( error.message );
+
+					} );
+
+				disqueue.request( 'v1.users.get', 'YAmii' + i , 0 )
+					.then( function( yami ) {
+						console.log( colors.green( '----------SUCCESS-----------' ) );
+						console.log( yami );
+						console.log( colors.green( '----------SUCCESS-----------' ) );
+					} );
+
+			}
+
+		} );
+
+	} );
---
+++ example/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Disqueue = require( '../lib' );
+
+new Disqueue()
+	.then( function( disqueue ) {
+
+		disqueue.registerListeners( [ 'v1.users.get', 'v1.users.save', 'v1.users.list' ] );
+
+		disqueue.respond( 'v1.users.save', function ( message, send ) {
+
+			return disqueue.request( 'v1.users.list', 'HI', 0 )
+				.then( function ( d ) {
+					send( null, message + ' Gideonairex' + d );
+				} );
+
+		} );
+
+		disqueue.respond( 'v1.users.get', function ( message, send ) {
+
+			setTimeout( function() {
+				send( null, message );
+			}, Math.random() * 1000 );
+
+		} );
+
+		disqueue.respond( 'v1.users.list', function ( message, send ) {
+
+			setTimeout( function() {
+				send( null, message + ' and BYE' );
+			}, Math.random() * 1000 );
+
+		} );
+
+
+	} );
