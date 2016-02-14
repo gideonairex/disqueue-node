3d30b99d9b0ef698a46ae24c6788b04f7cd395dd
Chris Kinsman <chris@kinsman.net>
2016-02-13 18:41:20 -0800

    Add tests

---
+++ test/commands-list/working.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Disqueue = require( '../../' );
+
+require( 'should' );
+
+describe( '"WORKING"', function () {
+    describe( 'successful working', function () {
+        var disqueue;
+        var data;
+
+        before( function ( done ) {
+            disqueue = new Disqueue();
+            disqueue.on( 'connected', function () {
+                done();
+            } );
+        } );
+
+        before( function ( done ) {
+            disqueue.addJob( {
+                'queue' : 'working-job-test',
+                'job'   : 'Hello world'
+            }, function ( error, result ) {
+                disqueue.working( result, function ( errorWorking, resultWorking ) {
+                    data = resultWorking;
+                    done();
+                } );
+            } );
+        } );
+
+        it( 'should return true', function () {
+            data.should.equal( 1 );
+        } );
+    } );
+} );
